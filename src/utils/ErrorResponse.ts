dc6e27444c8afc62dd2b7d1033c1bd4bcec50477
suvasis <suvasis02@kgpian.iitkgp.ac.in>
2024-11-28 21:48:40 +0530

    add error and response class

---
+++ src/utils/ErrorResponse.ts
@@ -0,0 +1,23 @@
+import { ErrorCode } from "./types";
+
+class ApiError extends Error {
+  statusCode: number;
+  success: boolean;
+  errorCode: ErrorCode;
+
+  constructor(
+    statusCode: number,
+    message = "Something went wrong",
+    errorCode: ErrorCode
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.success = false;
+    this.errorCode = errorCode;
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { ApiError };
