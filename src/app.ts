e2bc2f6c450326e78e8d4602f2cc58b52d154708
suvasis <suvasis02@kgpian.iitkgp.ac.in>
2024-11-28 18:10:43 +0530

    create user route

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.router";
 
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/v1/user", userRouter);
 
 export { app };
---
+++ src/routes/user.router.ts
@@ -0,0 +1,8 @@
+import Router from "express";
+
+const router = Router();
+
+router.route("/register").post();
+router.route("/login").post();
+
+export default router;
